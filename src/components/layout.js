36973958ef1d1ca2a3f73e9a54841da71803eafb
Josua Blejeru <josua.blejeru@gmail.com>
2020-09-20 00:18:30 +0200

    style components with tailwind

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import tw from "twin.macro"
+
 import Header from "./header"
 import "./layout.css"
 
@@ -26,20 +28,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div tw="ml-4">
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        <footer tw="mt-4">
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.com">Gatsby</a> by Josua Blejeru
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import tw from "twin.macro"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,14 +9,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 tw="py-8 mt-8 max-w-sm rounded-lg overflow-hidden shadow-2xl text-center hover:bg-gray-300">
+      Welcome to my Starter!
+    </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
